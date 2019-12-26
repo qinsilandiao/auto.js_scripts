4d0f82b3f77c6919c7e2f5ba19b255c4623c2180
qinsilandiao <1123960590@qq.com>
2019-12-26 15:54:49 +0800

    Update douyin.js
    弹出青少年窗口点击优化，随机点赞优化

--- douyin.js
+++ douyin.js
@@ -1,22 +1,34 @@
 auto.waitFor();
 app.launchApp('抖音极速版')
 sleep(5000);
-i=1;
-while(true){
-    toast("滑动"+i+'次')
+youngWin();
+i = 1;
+while (true) {
+    toast("滑动" + i + '次')
     i++;
-    index1=random(300,800);
-    index2=random(1600,1800);
-    index3=random(300,800);
-    index4=random(500,700);
-    timeGo=random(250,600);
-    timeNext=random(4000,7000);
-    /**heartRandom=random(1,5);
-    if(heartRandom==3){
-        heart =id('a4n').findOne().bounds();
-        click(heart.centerX(), heart.centerY());
-        sleep(2000)
-    }**/
-    swipe(index1,index2 ,index3 ,index4 ,timeGo );
+    index1 = random(300, 800);
+    index2 = random(1600, 1800);
+    index3 = random(300, 800);
+    index4 = random(500, 700);
+    timeGo = random(250, 600);
+    timeNext = random(4000, 7000);
+    heartClick();
+    swipe(index1, index2, index3, index4, timeGo);
     sleep(timeNext)
+}
+
+/**如果弹出青少年窗口，点击 */
+function youngWin() {
+    if (youngWin = text("我知道了").exists()) {
+        youngWin.click();
+    };
+}
+
+/**随机点赞并休息一秒 */
+function heartClick(){
+    index=random(1,5);
+    if(index==3){
+        id('a4m').findOnce().click();
+    }
+    sleep(1000);
 }
